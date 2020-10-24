a179df0d9a4ec1d17534205f31c768d33b3e5ca1
GiacomoGitHub <poddagiacomo@gmail.com>
2020-10-24 13:52:01 +0200

    Updated the button and inputs

--- script.js
+++ script.js
@@ -1,37 +1,31 @@
 
-let antifragile = new Book ("Antifragile", "Nassim Taleb", "544"); // we use this Book Object as a sample to see how to make Books appear on screen
+let antifragile = new Book ("Antifragile", "Nassim Nicholas Taleb", "544"); // this Book Object is a sample to test the code
 
-let harryPotter1 = new Book ("Harry Potter and the Philosopher's Stone", "J.K.Rowling", "342"); // we use this Book Object as a sample to see how to make Books appear on screen
-
-
-let myLibrary = [antifragile, harryPotter1]; // it's an empty array that stores our Book objects
+let harryPotter1 = new Book ("Harry Potter and the Philosopher's Stone", "J.K.Rowling", "342"); // // this Book Object is a sample to test the code
 
 
-function Book(title, author, pages) { // it's the constructor that creates new Book Objects
+let myLibrary = [antifragile, harryPotter1]; // empty array that stores Book objects
+
+
+function Book(title, author, pages) { // constructor that creates new Book Objects
     this.title = title
     this.author = author
-    this.pages = pages
+    this.pages = pages + " pages"
 }
 
 
 function addBookToLibrary(title, author, pages) { // takes the user's input
-    let newBook = new Book (title, author, pages); // it uses it to create a new Object
-    myLibrary.push(newBook); // adds new Book Object we just created with the constructor to the array myLibrary
+    let newBook = new Book (title, author, pages); // uses the input to create a new Object
+    myLibrary.push(newBook); // adds new Book Object created with the constructor to the array myLibrary
 }
 
-let demo = document.getElementById("demo"); // this targets the <p> where we will display the Book Objects
+let bookList = document.getElementById("bookList"); // targets the <p> to display the Book Objects
 
-myLibrary.forEach(item => demo.innerHTML = Object.values(item).join(" ")); // this should show the Book Objects, but it's not working yet
+myLibrary.forEach(item => bookList.innerHTML += "- " + Object.values(item).join(", ") + "<br>" + "<br>"); // shows the Book Objects
 
-
-
-// Some books to test our code:
+// Books to test the code:
 // ("Harry Potter and the Philosopher's Stone", "J.K.Rowling", "342")
 // ("Harry Potter and the Chamber of Secrets", "J.K.Rowling", "251")
 // ("Harry Potter and the Prisoner of Azkaban", "J.K.Rowling", "317")
 // ("The Lord of the Rings: The Fellowship of the Ring", "J.R.Tolkien", "479")
 // ("The Lord of the Rings: The Return of the King", "J.R.Tolkien", "347")
-
-// possible way to show books on page
-// let libraryDisplayer = document.getElementById("libraryDisplayer");
-// myLibrary.forEach(item => libraryDisplayer.innerHTML += "<br>" + Object.values(item).join(" "));
