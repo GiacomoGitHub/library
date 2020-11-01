30b5efa571c07043c4cdeb2c2494fbb6ab63ce5c
GiacomoGitHub <poddagiacomo@gmail.com>
2020-11-01 17:58:03 +0100

    User can submit input pressing enter key

--- script.js
+++ script.js
@@ -23,23 +23,65 @@ function addBookToLibrary() { // takes the user's input
 
 function updateLibrary() { // update the library
     let displayer = document.getElementById("displayer"); // targets the displayer
-    displayer.innerHTML = "MY BOOKS"; // clears the library
-    myLibrary.forEach(item => { // for each Object in the array to the following
+    displayer.innerHTML = "MY BOOKS" + "<br>" + "<br>" + "Use the checkbox to remember which books you have read"; // clears the library and adds the title of the displayer
+    myLibrary.forEach(item => { // for each Object in the array do the following
         bookList = document.createElement("div"); // create a new div
         bookList.setAttribute("id", "bookList"); // give it the id bookList for styling
-        bookList.innerHTML += Object.values(item).join(", ") + "<br>" + "<br>"; // show the Object inside it
+        let toggle = document.createElement("input"); // creates the toggle
+        toggle.setAttribute("id", "toggle"); // sets its id
+        toggle.setAttribute("type", "checkbox"); // makes it as a checkbox
+        bookList.appendChild(toggle); // appends it to the new div
+        bookList.innerHTML += Object.values(item).join(", ") + "<br>" + "<br>"; // show the Object inside the div
         displayer.appendChild(bookList); // append the new div to the DOM
-        let toggle = document.createElement("input");
-        toggle.setAttribute("id", "toggle");
-        toggle.setAttribute("type", "checkbox");
-        bookList.appendChild(toggle);
+        let removeBtn = document.createElement("button"); // creates the remove button
+        removeBtn.setAttribute("id", "removeBtn"); // sets its id
+        removeBtn.innerHTML = "&#10005" // sets the content inside the button
+        bookList.appendChild(removeBtn); // appends it to the new div
     });
 }
 
-document.getElementById("btn").addEventListener('click', addBookToLibrary); // listens to click on button and calls addBookToLibrary
+let addButton = document.getElementById("buttonAddToLibrary"); // targets the "Add book to library" button
+addButton.addEventListener('click', addBookToLibrary); // listens to click on button and calls addBookToLibrary
+
+
+let inputFieldTitle = document.getElementById("title"); // targets the title input
+inputFieldTitle.addEventListener('keydown', function(event) {
+    if (event.key === "Enter") { // If the key "Enter" is pressed
+      event.preventDefault(); // Cancel the default action, if needed
+      document.getElementById("buttonAddToLibrary").click(); // Trigger the button element with a click
+    }
+  });
+
+
+let inputFieldAuthor = document.getElementById("author"); // targets the author input
+inputFieldAuthor.addEventListener('keydown', function(event) {
+    if (event.key === "Enter") { // If the key "Enter" is pressed
+      event.preventDefault(); // Cancel the default action, if needed
+      document.getElementById("buttonAddToLibrary").click(); // Trigger the button element with a click
+    }
+  });
+
+
+let inputFieldPages = document.getElementById("pages"); // targets the pages input
+inputFieldPages.addEventListener('keydown', function(event) {
+    if (event.key === "Enter") { // If the key "Enter" is pressed
+      event.preventDefault(); // Cancel the default action, if needed
+      document.getElementById("buttonAddToLibrary").click(); // Trigger the button element with a click
+    }
+  });
+
 
 function clearInputFields() { // resets the input after pressing the button
     document.getElementById("title").value = title.defaultValue;
     document.getElementById("author").value = author.defaultValue;
     document.getElementById("pages").value = pages.defaultValue;
 }  
+
+
+
+
+// THINGS I'D LIKE TO HAVE
+// don't submit if empty field and show me where I need to add stuff
+// dark/light mode switch
+// books stored in alphabetical order
+// possibility to see only books I haven't read yet
